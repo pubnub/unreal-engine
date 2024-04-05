476a59129af3a271092295c11544d2d59ce08ab5
Kamil Gronek <47022390+KGronek@users.noreply.github.com>
2024-04-05 11:25:29 +0200

    Add async Publish and Subscribe functionality to C++ wrapper. (#2)

--- Source/PubnubLibrary/Private/PubnubSubsystem.cpp
+++ Source/PubnubLibrary/Private/PubnubSubsystem.cpp
@@ -5,6 +5,7 @@
 #include "Threads/PubnubFunctionThread.h"
 #include "Threads/PubnubLoopingThread.h"
 
+DEFINE_LOG_CATEGORY_STATIC(PubnubLog, Log, All)
 
 void UPubnubSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
@@ -30,7 +31,7 @@ void UPubnubSubsystem::Deinitialize()
 
 void UPubnubSubsystem::InitPubnub()
 {
-	if(!PublishThread)
+	if(!CheckPublishThreadValidity())
 	{return;}
 
 	PublishThread->AddFunctionToQueue( [this]
@@ -41,7 +42,7 @@ void UPubnubSubsystem::InitPubnub()
 
 void UPubnubSubsystem::DeinitPubnub()
 {
-	if(!PublishThread)
+	if(!CheckPublishThreadValidity())
 	{return;}
 	
 	PublishThread->AddFunctionToQueue( [this]
@@ -52,7 +53,7 @@ void UPubnubSubsystem::DeinitPubnub()
 
 void UPubnubSubsystem::SetUserID(FString UserID)
 {
-	if(!PublishThread)
+	if(!CheckPublishThreadValidity())
 	{return;}
 	
 	PublishThread->AddFunctionToQueue( [this, UserID]
@@ -61,11 +62,165 @@ void UPubnubSubsystem::SetUserID(FString UserID)
 	});
 }
 
+void UPubnubSubsystem::SetSecretKey()
+{
+	if(!CheckPublishThreadValidity())
+	{return;}
+	
+	PublishThread->AddFunctionToQueue( [this]
+	{
+		SetSecretKey_priv();
+	});
+}
+
+void UPubnubSubsystem::PublishMessage(FString ChannelName, FString Message, FPubnubPublishSettings PublishSettings)
+{
+	if(!CheckPublishThreadValidity())
+	{return;}
+	
+	PublishThread->AddFunctionToQueue( [this, ChannelName, Message, PublishSettings]
+	{
+		PublishMessage_priv(ChannelName, Message, PublishSettings);
+	});
+}
+
+void UPubnubSubsystem::SubscribeToChannel(FString ChannelName)
+{
+	if(!CheckPublishThreadValidity())
+	{return;}
+	
+	PublishThread->AddFunctionToQueue( [this, ChannelName]
+	{
+		SubscribeToChannel_priv(ChannelName);
+	});
+}
+
+void UPubnubSubsystem::SubscribeToGroup(FString GroupName)
+{
+	if(!CheckPublishThreadValidity())
+	{return;}
+	
+	PublishThread->AddFunctionToQueue( [this, GroupName]
+	{
+		SubscribeToGroup_priv(GroupName);
+	});
+}
+
+void UPubnubSubsystem::UnsubscribeFromChannel(FString ChannelName)
+{
+	if(!CheckPublishThreadValidity())
+	{return;}
+	
+	PublishThread->AddFunctionToQueue( [this, ChannelName]
+	{
+		UnsubscribeFromChannel_priv(ChannelName);
+	});
+}
+
+void UPubnubSubsystem::UnsubscribeFromGroup(FString GroupName)
+{
+	if(!CheckPublishThreadValidity())
+	{return;}
+	
+	PublishThread->AddFunctionToQueue( [this, GroupName]
+	{
+		UnsubscribeFromGroup_priv(GroupName);
+	});
+}
+
+void UPubnubSubsystem::UnsubscribeFromAll()
+{
+	if(!CheckPublishThreadValidity())
+	{return;}
+	
+	PublishThread->AddFunctionToQueue( [this]
+	{
+		UnsubscribeFromAll_priv();
+	});
+}
+
+void UPubnubSubsystem::SystemPublish()
+{
+	if(SubscribedChannels.IsEmpty())
+	{return;}
+
+	//TODO: this will not unlock context if user is subscribed only to groups, but not to any channels. This issue needs to be addressed.
+	PublishMessage(SubscribedChannels[0], "{\"system\":\"subscribe unlock message\"}");
+}
+
+void UPubnubSubsystem::StartPubnubSubscribeLoop()
+{
+	if(!SubscribeThread)
+	{return;}
+
+	SubscribeThread->AddLoopingFunction([this]
+	{
+		if(SubscribedChannels.IsEmpty() && SubscribedGroups.IsEmpty())
+		{return;}
+
+		UE_LOG(PubnubLog, Warning, TEXT("Pubnub subscribe"));
+		//Subscribe to channels - this is blocking function
+		pubnub_subscribe(ctx_sub, TCHAR_TO_ANSI(*StringArrayToCommaSeparated(SubscribedChannels)), TCHAR_TO_ANSI(*StringArrayToCommaSeparated(SubscribedGroups)));
+
+		//TODO: Make sure it works and maybe come up with more sophisticated check
+		//If this happens after closing program just return
+		if(!this)
+		{return;}
+
+		//Check for subscribe result
+		pubnub_res SubscribeResult = pubnub_await(ctx_sub);
+		if (SubscribeResult != PNR_OK)
+		{
+			FString ErrorCode(pubnub_res_2_string(SubscribeResult));
+			UE_LOG(PubnubLog, Error, TEXT("Failed to subscribe, error: %s"), *ErrorCode);
+			{return;}
+		}
+
+		//At this stage we received messages, so read them and get channel from where they were sent
+		const char* MessageChar = pubnub_get(ctx_sub);
+		const char* ChannelChar = pubnub_get_channel(ctx_sub);
+		while(MessageChar != NULL)
+		{
+			FString Message(MessageChar);
+			FString Channel(ChannelChar);
+
+			//Broadcast callback with message content
+			//Message needs to be called back on Game Thread
+			AsyncTask(ENamedThreads::GameThread, [this, Message, Channel]()
+			{
+				OnMessageReceived.Broadcast(Message, Channel);
+			});
+				
+			MessageChar = pubnub_get(ctx_sub);
+			ChannelChar = pubnub_get_channel(ctx_sub);
+		}
+	});
+}
+
+FString UPubnubSubsystem::StringArrayToCommaSeparated(TArray<FString> StringArray)
+{
+	FString CommaSeparatedString;
+	for(FString StringElement : SubscribedChannels)
+	{
+		if(CommaSeparatedString.IsEmpty())
+		{
+			CommaSeparatedString.Append(StringElement);
+		}
+		else
+		{
+			CommaSeparatedString.Append(",");
+			CommaSeparatedString.Append(StringElement);
+		}
+	}
+	return CommaSeparatedString;
+}
+
 void UPubnubSubsystem::LoadPluginSettings()
 {
 	//Save all settings
 	PubnubSettings = GetMutableDefault<UPubnubSettings>();
-	
+
+	//TODO: Make something better for reading keys - at least check size instead of hard-coding it.
 	//Copy memory for chars containing keys
 	memcpy_s(PublishKey, 42, TCHAR_TO_ANSI(*PubnubSettings->PublishKey), 42);
 	memcpy_s(SubscribeKey,42,  TCHAR_TO_ANSI(*PubnubSettings->SubscribeKey), 42);
@@ -75,6 +230,38 @@ void UPubnubSubsystem::LoadPluginSettings()
 	SecretKey[54] = '\0';
 }
 
+//This functions is a wrapper to IsInitialized bool, so it can print error if user is trying to do anything before initializing Pubnub correctly
+bool UPubnubSubsystem::CheckIsPubnubInitialized()
+{
+	if(!IsInitialized)
+	{
+		UE_LOG(PubnubLog, Error, TEXT("Pubnub is not initialized. Aborting operation"));
+	}
+	
+	return IsInitialized;
+}
+
+//This functions is a wrapper to IsUserIDSet bool, so it can print error if user is trying to do anything before setting user ID correctly
+bool UPubnubSubsystem::CheckIsUserIDSet()
+{
+	if(!IsUserIDSet)
+	{
+		UE_LOG(PubnubLog, Error, TEXT("Pubnub user ID is not set. Aborting operation"));
+	}
+	
+	return IsUserIDSet;
+}
+
+bool UPubnubSubsystem::CheckPublishThreadValidity()
+{
+	if(!PublishThread)
+	{
+		UE_LOG(PubnubLog, Error, TEXT("PublishThread is invalid. Aborting operation"));
+		return false;
+	}
+	
+	return true;
+}
 
 
 /* PRIV FUNCTIONS */
@@ -83,12 +270,29 @@ void UPubnubSubsystem::InitPubnub_priv()
 {
 	if(IsInitialized)
 	{return;}
+
+	//TODO::Not only check if keys are empty, but also make sure they are valid
+	//Make sure that keys are filled
+	if(std::strlen(PublishKey) == 0 )
+	{
+		UE_LOG(PubnubLog, Error, TEXT("Publish key is empty, can't initialize Pubnub"));
+	}
+
+	if(std::strlen(SubscribeKey) == 0 )
+	{
+		UE_LOG(PubnubLog, Error, TEXT("Subscribe key is empty, can't initialize Pubnub"));
+	}
 	
 	ctx_pub = pubnub_alloc();
 	ctx_sub = pubnub_alloc();
 
 	pubnub_init(ctx_pub, PublishKey, SubscribeKey);
 	pubnub_init(ctx_sub, PublishKey, SubscribeKey);
+
+	if(PubnubSettings->SetSecretKetAutomatically)
+	{
+		SetSecretKey();
+	}
 
 	IsInitialized = true;
 }
@@ -98,7 +302,8 @@ void UPubnubSubsystem::DeinitPubnub_priv()
 	if(!IsInitialized)
 	{return;}
 
-	//Add Unsubscribe from all channels here
+	//Unsubscribe from all channels so this user will not be visible for others anymore
+	UnsubscribeFromAll();
 	
 	if(ctx_pub)
 	{
@@ -115,9 +320,151 @@ void UPubnubSubsystem::DeinitPubnub_priv()
 
 void UPubnubSubsystem::SetUserID_priv(FString UserID)
 {
-	if(!IsInitialized)
+	if(!CheckIsPubnubInitialized())
 	{return;}
 
 	pubnub_set_user_id(ctx_pub, TCHAR_TO_ANSI(*UserID));
 	pubnub_set_user_id(ctx_sub, TCHAR_TO_ANSI(*UserID));
+
+	IsUserIDSet = true;
+}
+
+void UPubnubSubsystem::SetSecretKey_priv()
+{
+	if(!CheckIsPubnubInitialized())
+	{return;}
+
+	if(std::strlen(SecretKey) == 0)
+	{
+		UE_LOG(PubnubLog, Warning, TEXT("Can't set Secret Key. Secret Key is empty."));
+		return;
+	}
+
+	pubnub_set_secret_key(ctx_pub, SecretKey);
+	pubnub_set_secret_key(ctx_sub, SecretKey);
+}
+
+void UPubnubSubsystem::PublishMessage_priv(FString ChannelName, FString Message, FPubnubPublishSettings PublishSettings)
+{
+	if(!CheckIsPubnubInitialized() || !CheckIsUserIDSet())
+	{return;}
+
+	if(ChannelName.IsEmpty() || Message.IsEmpty())
+	{return;}
+
+	//TODO: move this to a separate function (needs a way to store global *char from FString)
+
+	//Set all options from PublishSettings
+	pubnub_publish_options PublishOptions = pubnub_publish_defopts();
+	PublishOptions.store = PublishSettings.StoreInHistory;
+	PublishOptions.replicate = PublishSettings.Replicate;
+	
+	if(!PublishSettings.MetaData.IsEmpty())
+	{
+		auto CharConverter = StringCast<ANSICHAR>(*PublishSettings.MetaData);
+		const char* MetaDataChar = CharConverter.Get();
+		PublishOptions.meta = MetaDataChar;
+	}
+
+	//Convert UE enum to pubnub_method enum
+	PublishOptions.method = (pubnub_method)(uint8)PublishSettings.PublishMethod;
+	
+	pubnub_publish_ex(ctx_pub, TCHAR_TO_ANSI(*ChannelName), TCHAR_TO_ANSI(*Message), PublishOptions);
+	pubnub_res PublishResult = pubnub_await(ctx_pub);
+
+	if(PublishResult != PNR_OK)
+	{
+		UE_LOG(PubnubLog, Error, TEXT("Publish failed. Error code: %d"), PublishResult);
+	}
+}
+
+void UPubnubSubsystem::SubscribeToChannel_priv(FString ChannelName)
+{
+	if(!CheckIsPubnubInitialized() || !CheckIsUserIDSet())
+	{return;}
+
+	if(ChannelName.IsEmpty())
+	{return;}
+
+	if(SubscribedChannels.Contains(ChannelName))
+	{return;}
+
+	//Check if Pubnub was already subscribed to a channel or a group.
+	bool WasCheckingMessages = !SubscribedChannels.IsEmpty() || !SubscribedGroups.IsEmpty();
+	
+	SubscribedChannels.Add(ChannelName);
+	
+	if(!WasCheckingMessages)
+	{
+		StartPubnubSubscribeLoop();
+	}
+
+	//System publish to unlock subscribe context and start listening for this new channel
+	SystemPublish();
+}
+
+void UPubnubSubsystem::SubscribeToGroup_priv(FString GroupName)
+{
+	if(!CheckIsPubnubInitialized() || !CheckIsUserIDSet())
+	{return;}
+
+	if(GroupName.IsEmpty())
+	{return;}
+
+	if(SubscribedGroups.Contains(GroupName))
+	{return;}
+
+	//Check if Pubnub was already subscribed to a channel or a group.
+	bool WasCheckingMessages = !SubscribedChannels.IsEmpty() || !SubscribedGroups.IsEmpty();
+	
+	SubscribedGroups.Add(GroupName);
+
+	if(WasCheckingMessages)
+	{
+		StartPubnubSubscribeLoop();
+	}
+
+	//System publish to unlock subscribe context and start listening for this new group
+	SystemPublish();
+}
+
+void UPubnubSubsystem::UnsubscribeFromChannel_priv(FString ChannelName)
+{
+	//make sure user was subscribed to that channel
+	if(SubscribedChannels.Remove(ChannelName) == 0)
+	{return;}
+
+	pubnub_leave(ctx_pub, TCHAR_TO_ANSI(*ChannelName), NULL);
+}
+
+void UPubnubSubsystem::UnsubscribeFromGroup_priv(FString GroupName)
+{
+	//make sure user was subscribed to that channel
+	if(SubscribedGroups.Remove(GroupName) == 0)
+	{return;}
+
+	pubnub_leave(ctx_pub, NULL, TCHAR_TO_ANSI(*GroupName));
+}
+
+void UPubnubSubsystem::UnsubscribeFromAll_priv()
+{
+	//TODO: Find out how to unsubscribe from all channels correctly
+	for(FString Channel : SubscribedChannels)
+	{
+		pubnub_leave(ctx_pub, TCHAR_TO_ANSI(*Channel), NULL);
+		pubnub_await(ctx_pub);
+	}
+
+	for(FString Group : SubscribedGroups)
+	{
+		pubnub_leave(ctx_pub, NULL, TCHAR_TO_ANSI(*Group));
+		pubnub_await(ctx_pub);
+	}
+	
+	pubnub_cancel(ctx_pub);
+
+	SubscribedChannels.Empty();
+	SubscribedGroups.Empty();
+	
+	SubscribeThread->ClearLoopingFunctions();
 }
--- Source/PubnubLibrary/Public/PubnubSubsystem.h
+++ Source/PubnubLibrary/Public/PubnubSubsystem.h
@@ -11,6 +11,36 @@ class UPubnubSettings;
 class FPubnubFunctionThread;
 class FPubnubLoopingThread;
 
+UENUM(BlueprintType)
+enum class EPubnubPublishMethod : uint8
+{
+	pubnubSendViaGET,
+	pubnubSendViaPOST,
+	pubnubUsePATCH,
+	pubnubSendViaPOSTwithGZIP,
+	pubnubUsePATCHwithGZIP,
+	pubnubUseDELETE
+};
+
+
+USTRUCT(BlueprintType)
+struct FPubnubPublishSettings
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere) bool StoreInHistory = true;
+	//TODO: CipherKey is deprecated, decide what to do (hide variable or mark is as deprecated)
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere) FString CipherKey = "";
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere) bool Replicate = true;
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere) FString MetaData = "";
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere) EPubnubPublishMethod PublishMethod = EPubnubPublishMethod::pubnubSendViaGET;
+};
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnMessageReceived, FString, MessageJson, FString, Channel);
+
+
+
 UCLASS()
 class PUBNUBLIBRARY_API UPubnubSubsystem : public UGameInstanceSubsystem
 {
@@ -21,24 +51,58 @@ public:
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 	virtual void Deinitialize() override;
 
+#pragma region DELEGATE VARIABLES
+	
+	UPROPERTY(BlueprintAssignable, Category = "Pubnub|Subscribe")
+	FOnMessageReceived OnMessageReceived;
+
+#pragma endregion
+
+
 #pragma region BLUEPRINT EXPOSED
 	
 	/* BLUEPRINT EXPOSED FUNCTIONS */
-
-	UFUNCTION(BlueprintCallable, Category = "Pubnub")
+	//These functions don't have actual logic, they just call corresponding private functions on Pubnub threads
+	
+	UFUNCTION(BlueprintCallable, Category = "Pubnub|Init")
 	void InitPubnub();
 	
-	UFUNCTION(BlueprintCallable, Category = "Pubnub")
+	UFUNCTION(BlueprintCallable, Category = "Pubnub|Init")
 	void DeinitPubnub();
 
-	UFUNCTION(BlueprintCallable, Category = "Pubnub")
+	UFUNCTION(BlueprintCallable, Category = "Pubnub|Users")
 	void SetUserID(FString UserID);
+
+	UFUNCTION(BlueprintCallable, Category = "Pubnub|Init")
+	void SetSecretKey();
+
+	/**
+	 * Publish message in Json format to a specified channel
+	 * @param ChannelName Channel to publish message to. Can't be empty.
+	 * @param Message Message to publish. Has to be in Json format.
+	 * @param PublishSettings Optional advanced publish settings
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Pubnub|Publish")
+	void PublishMessage(FString ChannelName, FString Message, FPubnubPublishSettings PublishSettings = FPubnubPublishSettings());
+
+	UFUNCTION(BlueprintCallable, Category = "Pubnub|Subscribe")
+	void SubscribeToChannel(FString ChannelName);
+
+	UFUNCTION(BlueprintCallable, Category = "Pubnub|Subscribe")
+	void SubscribeToGroup(FString GroupName);
+
+	UFUNCTION(BlueprintCallable, Category = "Pubnub|Subscribe")
+	void UnsubscribeFromChannel(FString ChannelName);
+
+	UFUNCTION(BlueprintCallable, Category = "Pubnub|Subscribe")
+	void UnsubscribeFromGroup(FString GroupName);
+	
+	UFUNCTION(BlueprintCallable, Category = "Pubnub|Subscribe")
+	void UnsubscribeFromAll();
 
 #pragma endregion
 	
 private:
-
-	bool IsInitialized = false;
 
 	//New threads to call all C-Core functions asynchronously
 	TObjectPtr<FPubnubFunctionThread> PublishThread = nullptr;
@@ -47,6 +111,19 @@ private:
 	//Pubnub contexts for publishing data and subscribing to channels
 	pubnub_t *ctx_pub = nullptr;
 	pubnub_t *ctx_sub = nullptr;
+
+	TArray<FString> SubscribedChannels;
+	TArray<FString> SubscribedGroups;
+	
+	//Publish to the first subscribed channel to unlock subscribe context
+	void SystemPublish();
+
+	//Register to PubnubLoopingThread function to check in loop for messages from subscribed channels and groups
+	void StartPubnubSubscribeLoop();
+
+	//Useful for subscribing into multiple channels/groups. Returns Strings in format String1,String2,...
+	FString StringArrayToCommaSeparated(TArray<FString> StringArray);
+
 
 #pragma region PLUGIN SETTINGS
 	
@@ -61,10 +138,34 @@ private:
 	
 	void LoadPluginSettings();
 
-#pragma endregion 
+#pragma endregion
+
+	/* INITIALIZATION CHECKS */
+	
+	bool IsInitialized = false;
+	bool IsUserIDSet = false;
+	bool CheckIsPubnubInitialized();
+	bool CheckIsUserIDSet();
+	bool CheckPublishThreadValidity();
 
 
+#pragma region PRIVATE FUNCTIONS
+
+	/* PRIVATE FUNCTIONS */
+	//These functions are called from "BLUEPRINT EXPOSED" functions on PubNub threads. They shouldn't be called directly on Game Thread.
+	
 	void InitPubnub_priv();
 	void DeinitPubnub_priv();
 	void SetUserID_priv(FString UserID);
+	void SetSecretKey_priv();
+	void PublishMessage_priv(FString ChannelName, FString Message, FPubnubPublishSettings PublishSettings = FPubnubPublishSettings());
+	void SubscribeToChannel_priv(FString ChannelName);
+	void SubscribeToGroup_priv(FString GroupName);
+	void UnsubscribeFromChannel_priv(FString ChannelName);
+	void UnsubscribeFromGroup_priv(FString GroupName);
+	void UnsubscribeFromAll_priv();
+
+#pragma endregion
+	
 };
+
