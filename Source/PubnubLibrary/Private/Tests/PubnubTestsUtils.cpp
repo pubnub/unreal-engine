187d488041ca77e5cfa4877a44c9972e07c075f0
Kamil Gronek <kamil.gronek@pubnub.com>
2025-05-09 13:55:47 +0200

    Add Publish and Subscribe E2E tests.

---
+++ Source/PubnubLibrary/Private/Tests/PubnubTestsUtils.cpp
@@ -0,0 +1,34 @@
+﻿#include "Tests/PubnubTestsUtils.h"
+#include "Tests/AutomationCommon.h"
+
+
+bool FPubnubAutomationTestBase::InitTest()
+{
+	//Initialize GameInstance and PubnubSubsystem
+	GameInstance = NewObject<UGameInstance>(GEngine);
+	GameInstance->InitializeStandalone();
+	
+	if (!TestNotNull("GameInstance exists", GameInstance))
+	{return false;}
+
+	PubnubSubsystem = GameInstance->GetSubsystem<UPubnubSubsystem>();
+	if (!TestNotNull(" Pubnub Subsystem exists", PubnubSubsystem))
+	{return false;}
+
+	return true;
+}
+
+void FPubnubAutomationTestBase::CleanUp()
+{
+	//Final clean up
+	ADD_LATENT_AUTOMATION_COMMAND(FDelayedFunctionLatentCommand([this]()
+	{
+		PubnubSubsystem->DeinitPubnub();
+	}, 0.2f));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FDelayedFunctionLatentCommand([this]()
+	{
+		GameInstance->Shutdown();
+	}, 0.2f));
+	ADD_LATENT_AUTOMATION_COMMAND(FEngineWaitLatentCommand(0.2f));
+}
---
+++ Source/PubnubLibrary/Public/Tests/PubnubTestsUtils.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+#include "Misc/AutomationTest.h"
+#include "Templates/Function.h"
+#include "PubnubSubsystem.h"
+
+
+
+
+class FWaitUntilLatentCommand : public IAutomationLatentCommand
+{
+public:
+	FWaitUntilLatentCommand(TFunction<bool()> InCondition, float InTimeoutSeconds)
+		: Condition(MoveTemp(InCondition))
+		, TimeoutSeconds(InTimeoutSeconds)
+		, ElapsedTime(0.0f)
+	{}
+
+	virtual bool Update() override
+	{
+		ElapsedTime += FApp::GetDeltaTime();
+		if (Condition())
+		{
+			return true; // Done
+		}
+		return ElapsedTime >= TimeoutSeconds; // Fail if timeout
+	}
+
+private:
+	TFunction<bool()> Condition;
+	float TimeoutSeconds;
+	float ElapsedTime;
+};
+
+/**
+ * This is dedicated class for all Pubnub automated tests.
+ * It has helper functions to initialize and clean up all required systems.
+ */
+
+class FPubnubAutomationTestBase: public FAutomationTestBase
+{
+	
+public:
+	FPubnubAutomationTestBase(const FString& InName, const bool bInComplexTask)
+		: FAutomationTestBase(InName, bInComplexTask)
+	{}
+
+	//Initializes systems required by the test. This has to be called at the beginning of every test.
+	bool InitTest();
+	//Cleans up test systems. Call this at the end of every test
+	void CleanUp();
+
+	UPubnubSubsystem* PubnubSubsystem = nullptr;
+	UGameInstance* GameInstance = nullptr;
+};
