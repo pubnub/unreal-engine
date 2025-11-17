ff3c6fef173aafc04d32f93e0722b92fc181ee60
Kamil Gronek <kamil.gronek@pubnub.com>
2025-11-17 15:16:12 +0100

    Add basic Client intialization flow. Copy required functions and variables to handle subscriptions.

--- Source/PubnubLibrary/Private/PubnubClient.cpp
+++ Source/PubnubLibrary/Private/PubnubClient.cpp
@@ -2,7 +2,24 @@
 
 
 #include "PubnubClient.h"
+#include "PubNub.h"
 #include "PubnubSubsystem.h"
+#include "Threads/PubnubFunctionThread.h"
+#include "FunctionLibraries/PubnubUtilities.h"
+
+
+struct CCoreSubscriptionCallback
+{
+	pubnub_subscribe_message_callback_t Callback;
+	pubnub_subscription_t* Subscription;
+};
+
+//TODO:: Move this to logger
+static void PubnubSDKLogConverter(enum pubnub_log_level log_level, const char* message)
+{
+	
+}
+
 
 void UPubnubClient::DestroyClient()
 {
@@ -12,17 +29,176 @@ void UPubnubClient::DestroyClient()
 	PubnubSubsystem->DestroyPubnubClient(this);
 }
 
+void UPubnubClient::SetUserID(FString UserID)
+{
+}
+
+FString UPubnubClient::GetUserID()
+{
+	return "";
+}
+
+void UPubnubClient::SetSecretKey()
+{
+}
+
+void UPubnubClient::PublishMessage(FString Channel, FString Message,
+	FPubnubOnPublishMessageResponse OnPublishMessageResponse, FPubnubPublishSettings PublishSettings)
+{
+}
+
+void UPubnubClient::PublishMessage(FString Channel, FString Message,
+	FPubnubOnPublishMessageResponseNative NativeCallback, FPubnubPublishSettings PublishSettings)
+{
+}
+
+void UPubnubClient::PublishMessage(FString Channel, FString Message, FPubnubPublishSettings PublishSettings)
+{
+}
+
 void UPubnubClient::InitWithConfig(UPubnubSubsystem* InPubnubSubsystem, FPubnubConfig InConfig, int InClientID, FString InDebugName )
 {
 	PubnubSubsystem = InPubnubSubsystem;
 	ClientID = InClientID;
 	DebugName = InDebugName;
 
-	IsInitialized = true;
+	SavePubnubConfig(InConfig);
+	
+	InitPubnub_priv(InConfig);
+
+	//If initialized correctly, create required thread.
+	if(IsInitialized)
+	{
+		//Create new thread to queue all pubnub operations
+		PubnubCallsThread = new FPubnubFunctionThread;
+	}
 }
 
 void UPubnubClient::DeinitializeClient()
 {
 	IsInitialized = false;
 	PubnubSubsystem = nullptr;
+}
+
+void UPubnubClient::SavePubnubConfig(const FPubnubConfig& InConfig)
+{
+	PubnubConfig = InConfig;
+	
+	// Safely copy all keys using the utility function
+	UPubnubUtilities::SafeCopyFStringToCharBuffer(PublishKey, PublishKeySize + 1, InConfig.PublishKey, TEXT("PublishKey"));
+	UPubnubUtilities::SafeCopyFStringToCharBuffer(SubscribeKey, PublishKeySize + 1, InConfig.SubscribeKey, TEXT("SubscribeKey"));
+	UPubnubUtilities::SafeCopyFStringToCharBuffer(SecretKey, SecretKeySize + 1, InConfig.SecretKey, TEXT("SecretKey"));
+}
+
+void UPubnubClient::PubnubError(FString ErrorMessage, EPubnubErrorType ErrorType)
+{
+}
+
+void UPubnubClient::PubnubResponseError(pubnub_res PubnubResponse, FString ErrorMessage)
+{
+}
+
+void UPubnubClient::OnCCoreSubscriptionStatusReceived(int StatusEnum, const void* StatusData)
+{
+	//Cast data back to C-Core types
+	pubnub_subscription_status status = static_cast<pubnub_subscription_status>(StatusEnum);
+	const pubnub_subscription_status_data_t* status_data = static_cast<const pubnub_subscription_status_data_t*>(StatusData);
+
+	//Call and remove the first subscription result delegate in queue
+	if(!SubscriptionResultDelegates.IsEmpty())
+	{
+		FPubnubOperationResult Result;
+		Result.Error = status == PNSS_SUBSCRIPTION_STATUS_CONNECTION_ERROR || status == PNSS_SUBSCRIPTION_STATUS_DISCONNECTED_UNEXPECTEDLY;
+		Result.Status = Result.Error ? 503 : 200;
+		Result.ErrorMessage = pubnub_res_2_string(status_data->reason);
+		SubscriptionResultDelegates[0].ExecuteIfBound(Result);
+
+		SubscriptionResultDelegates.RemoveAt(0);
+	}
+
+	//Unlock the thread to proceed with queue
+	PubnubCallsThread->UnlockAfterSubscriptionOperationFinished();
+	
+	//Don't waste resources to translate data if there is no delegate bound to it
+	if(!OnSubscriptionStatusChanged.IsBound() && !OnSubscriptionStatusChangedNative.IsBound())
+	{return;}
+
+	FPubnubSubscriptionStatusData SubscriptionStatusData;
+	SubscriptionStatusData.Reason = pubnub_res_2_string(status_data->reason);
+
+	//If status is disconnected we don't need to give subscribed channels
+	if(status != PNSS_SUBSCRIPTION_STATUS_DISCONNECTED)
+	{
+		//Fill channels and channel groups data, from C-Core
+		if (NULL != status_data->channels)
+		{
+			FUTF8ToTCHAR Converter(status_data->channels);
+			FString Channels(Converter.Length(), Converter.Get());
+			Channels.ParseIntoArray(SubscriptionStatusData.Channels, TEXT(","));
+		}
+		if (NULL != status_data->channel_groups)
+		{
+			FUTF8ToTCHAR Converter(status_data->channel_groups);
+			FString ChannelGroups(Converter.Length(), Converter.Get());
+			ChannelGroups.ParseIntoArray(SubscriptionStatusData.ChannelGroups, TEXT(","));
+		}
+	}
+
+	//Call SubscriptionStatusChanged delegates 
+	OnSubscriptionStatusChanged.Broadcast((EPubnubSubscriptionStatus)status, SubscriptionStatusData);
+	OnSubscriptionStatusChangedNative.Broadcast((EPubnubSubscriptionStatus)status, SubscriptionStatusData);
+}
+
+void UPubnubClient::InitPubnub_priv(const FPubnubConfig& Config)
+{
+	if(IsInitialized)
+	{return;}
+	
+	//Make sure that keys are filled
+	if(PublishKey[0] == '\0')
+	{
+		PubnubError("Publish key is empty, can't initialize Pubnub");
+		return;
+	}
+
+	if(SubscribeKey[0] == '\0')
+	{
+		PubnubError("Subscribe key is empty, can't initialize Pubnub");
+		return;
+	}
+	
+	ctx_pub = pubnub_alloc();
+	ctx_ee = pubnub_alloc();
+
+	//Send logging callback to Pubnub sdk, so we can pass all logs to UE
+	pubnub_set_log_callback(PubnubSDKLogConverter);
+	
+	pubnub_enforce_api(ctx_pub, PNA_SYNC);
+	pubnub_enforce_api(ctx_ee, PNA_CALLBACK);
+
+	pubnub_init(ctx_pub, PublishKey, SubscribeKey);
+	pubnub_init(ctx_ee, PublishKey, SubscribeKey);
+
+	pubnub_subscribe_status_callback_t Callback = +[](const pubnub_t *pb, const pubnub_subscription_status status, const pubnub_subscription_status_data_t status_data, void* _data)
+	{
+		UPubnubClient* ThisClient = static_cast<UPubnubClient*>(_data);
+		if(!ThisClient)
+		{return;}
+
+		ThisClient->OnCCoreSubscriptionStatusReceived(status, &status_data);
+	};
+	//Register subscription status listener with callback created above
+	pubnub_subscribe_add_status_listener(ctx_ee, Callback, this);
+	
+	IsInitialized = true;
+
+	if(!Config.UserID.IsEmpty())
+	{
+		this->SetUserID(Config.UserID);
+	}
+	
+	if(PubnubConfig.SetSecretKeyAutomatically)
+	{
+		SetSecretKey();
+	}
 }
--- Source/PubnubLibrary/Public/PubnubClient.h
+++ Source/PubnubLibrary/Public/PubnubClient.h
@@ -3,11 +3,29 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/Object.h"
 #include "PubnubStructLibrary.h"
+#include "PubnubEnumLibrary.h"
+#include "UObject/Object.h"
 #include "PubnubClient.generated.h"
 
 class UPubnubSubsystem;
+class FPubnubFunctionThread;
+struct CCoreSubscriptionCallback;
+
+
+struct pubnub_;
+typedef struct pubnub_ pubnub_t;
+enum pubnub_res;
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FPubnubOnSubscriptionStatusChanged, EPubnubSubscriptionStatus, Status, FPubnubSubscriptionStatusData, StatusData);
+DECLARE_MULTICAST_DELEGATE_TwoParams(FPubnubOnSubscriptionStatusChangedNative, EPubnubSubscriptionStatus Status, const FPubnubSubscriptionStatusData& StatusData);
+
+
+DECLARE_DYNAMIC_DELEGATE_TwoParams(FPubnubOnPublishMessageResponse, FPubnubOperationResult, Result, FPubnubMessageData, PublishedMessage);
+DECLARE_DELEGATE_TwoParams(FPubnubOnPublishMessageResponseNative, const FPubnubOperationResult& Result, const FPubnubMessageData& PublishedMessage);
+DECLARE_DYNAMIC_DELEGATE_OneParam(FPubnubOnSubscribeOperationResponse, FPubnubOperationResult, Result);
+DECLARE_DELEGATE_OneParam(FPubnubOnSubscribeOperationResponseNative, const FPubnubOperationResult& Result);
 
 /**
  * 
@@ -20,6 +38,13 @@ class PUBNUBLIBRARY_API UPubnubClient : public UObject
 	friend class UPubnubSubsystem;
 
 public:
+
+	/**Listener to react for subscription status changed */
+	UPROPERTY(BlueprintAssignable, Category = "Pubnub|Delegates")
+	FPubnubOnSubscriptionStatusChanged OnSubscriptionStatusChanged;
+
+	/**Listener to react for subscription status changed , equivalent that accepts lambdas*/
+	FPubnubOnSubscriptionStatusChangedNative OnSubscriptionStatusChangedNative;
 	
 
 	UFUNCTION(BlueprintCallable, Category="PubnubClient")
@@ -28,14 +53,120 @@ public:
 	UFUNCTION(BlueprintCallable, Category="PubnubClient")
 	void DestroyClient();
 
+
+	/**
+	 * Sets the user ID for the current session.
+	 * 
+	 * @param UserID The user ID to set.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Pubnub|General")
+	void SetUserID(FString UserID);
+
+	/**
+	 * Gets the current user ID.
+	 * 
+	 * @return The current user ID.
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Pubnub|General")
+	FString GetUserID();
+
+	/**
+	 * Sets the secret key for the PubNub account. Uses SecretKey provided in plugin settings.
+	 * Don't call it manually if "SetSecretKeyAutomatically" in plugin settings is set to true.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Pubnub|AccessManager")
+	void SetSecretKey();
+
+	/**
+	 * Publishes a message to a specified channel.
+	 * 
+	 * @param Channel The ID of the channel to publish the message to.
+	 * @param Message The message to publish. This message can be any data type that can be serialized into JSON.
+	 * @param OnPublishMessageResponse Optional delegate to listen for the publish result.
+	 * @param PublishSettings Optional settings for the publish operation. See FPubnubPublishSettings for more details.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Pubnub|Publish", meta = (AutoCreateRefTerm = "OnPublishMessageResponse"))
+	void PublishMessage(FString Channel, FString Message, FPubnubOnPublishMessageResponse OnPublishMessageResponse, FPubnubPublishSettings PublishSettings = FPubnubPublishSettings());
+
+	/**
+	 * Publishes a message to a specified channel.
+	 * 
+	 * @param Channel The ID of the channel to publish the message to.
+	 * @param Message The message to publish. This message can be any data type that can be serialized into JSON.
+	 * @param NativeCallback Optional delegate to listen for the publish result. Delegate in native form that can accept lambdas.
+	 *						 Can be skipped if publish result is not needed.
+	 * @param PublishSettings Optional settings for the publish operation. See FPubnubPublishSettings for more details.
+	 */
+	void PublishMessage(FString Channel, FString Message, FPubnubOnPublishMessageResponseNative NativeCallback = nullptr, FPubnubPublishSettings PublishSettings = FPubnubPublishSettings());
+
+	/**
+	 * Publishes a message to a specified channel. Overload without delegate to get publish result.
+	 * 
+	 * @param Channel The ID of the channel to publish the message to.
+	 * @param Message The message to publish. This message can be any data type that can be serialized into JSON.
+	 * @param PublishSettings Optional settings for the publish operation. See FPubnubPublishSettings for more details.
+	 */
+	void PublishMessage(FString Channel, FString Message, FPubnubPublishSettings PublishSettings);
+
+
+
+
+	
+
 private:
+
+	//Thread for all PubNub operations, this thread will queue all PubNub calls and trigger them one by one
+	FPubnubFunctionThread* PubnubCallsThread = nullptr;
+
+	//Pubnub context for the most of the pubnub operations
+	pubnub_t *ctx_pub = nullptr;
+	//Pubnub context for the event engine - subscribe operations
+	pubnub_t *ctx_ee = nullptr;
+
+#pragma region PUBNUB INIT
 
 	void InitWithConfig(UPubnubSubsystem* InPubnubSubsystem, FPubnubConfig InConfig, int InClientID, FString InDebugName = "");
 	void DeinitializeClient();
-
+	
 	UPROPERTY()
 	TObjectPtr<UPubnubSubsystem> PubnubSubsystem = nullptr;
 	int ClientID = -1;
-	bool IsInitialized = false;
 	FString DebugName = "";
+	bool IsInitialized = false;
+
+#pragma endregion 
+
+#pragma region PUBNUB CONFIG
+
+	//Container for all configuration settings
+	//TODO:: DO we event need to save config??
+	UPROPERTY()
+	FPubnubConfig PubnubConfig;
+
+	void SavePubnubConfig(const FPubnubConfig &InConfig);
+	
+	//Containers for keys stored from settings
+	static const int PublishKeySize = 42;
+	static const int SecretKeySize = 54;
+	char PublishKey[PublishKeySize + 1] = {};
+	char SubscribeKey[PublishKeySize + 1] = {};
+	char SecretKey[SecretKeySize + 1] = {};
+
+#pragma endregion 
+
+	//TODO:: Move these functions to the logger
+	void PubnubError(FString ErrorMessage, EPubnubErrorType ErrorType = EPubnubErrorType::PET_Error);
+	void PubnubResponseError(pubnub_res PubnubResponse, FString ErrorMessage);
+
+	//Storage for global subscriptions (not from Entities)
+	TMap<FString, CCoreSubscriptionCallback*> ChannelSubscriptions;
+	TMap<FString, CCoreSubscriptionCallback*> ChannelGroupSubscriptions;
+
+	//Array storing delegates for all queued subscription operations
+	TArray<FPubnubOnSubscribeOperationResponseNative> SubscriptionResultDelegates;
+
+	void OnCCoreSubscriptionStatusReceived(int StatusEnum, const void* StatusData);
+
+	
+	void InitPubnub_priv(const FPubnubConfig& Config);
 };
