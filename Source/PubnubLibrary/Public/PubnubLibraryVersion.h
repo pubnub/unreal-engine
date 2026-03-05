f5d49d1cbfd38444b93382f197d8bb6c8705e431
Kamil Gronek <kamil.gronek@pubnub.com>
2026-03-05 17:56:40 +0100

    Add version file.

---
+++ Source/PubnubLibrary/Public/PubnubLibraryVersion.h
@@ -0,0 +1,9 @@
+// Copyright 2025 PubNub Inc. All Rights Reserved.
+
+#pragma once
+
+/** Keep in sync with VersionName in PubnubLibrary.uplugin. Used by dependent plugins (e.g. PubnubChat) for version checks. Two digits per component (max 99 each). */
+#define PUBNUB_LIBRARY_VERSION_MAJOR 1
+#define PUBNUB_LIBRARY_VERSION_MINOR 2
+#define PUBNUB_LIBRARY_VERSION_PATCH 0
+#define PUBNUB_LIBRARY_VERSION ((PUBNUB_LIBRARY_VERSION_MAJOR * 10000) + (PUBNUB_LIBRARY_VERSION_MINOR * 100) + PUBNUB_LIBRARY_VERSION_PATCH)
